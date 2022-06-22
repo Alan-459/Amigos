d744a9859ae0efe6de2d1fc2dc2ace4018c6ab02
Alan-459 <52850770+Alan-459@users.noreply.github.com>
2022-06-22 22:34:36 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,19 @@
+
+from flask import Flask,render_template,url_for
+ 
+app = Flask(__name__)
+
+@app.route('/')
+@app.route('/home')
+def Home():
+    return render_template("mainhome.html")
+@app.route('/signup')
+def register():
+    return render_template("signup.html")
+
+@app.route('/signin')
+def signin():
+    return render_template("login.html")
+
+if __name__ =="__main__":
+    app.run(debug = True)
